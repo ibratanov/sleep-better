e56e8e1215589e25e9b39ac6e9330ac5f5423d6d
Ivo <4753372+ibratanov@users.noreply.github.com>
2023-10-01 13:45:00 -0700

    updates

--- sleep_agent.py
+++ sleep_agent.py
@@ -4,28 +4,12 @@ from env_vars_parser import get_env_var
 
 class SleepAgent:
     def get_response(self, sleep_data):
-        """
-        Calls the OpenAI 3.5 API to generate a response to the question.
-        The response is then matched to one of the answer choices and the index of the
-        matching answer choice is returned. If the response does not match any answer choice,
-        -1 is returned.
-
-        Args:
-            question (string): The question to be asked.
-            answer_choices (list of strings): A list of answer choices.
-
-        Returns:
-            int: The index of the answer choice that matches the response, or -1 if the response
-            does not match any answer choice.
-        """
-
         # Create the prompt.
         prompt = inspect.cleandoc(f"""
-            Q: {sleep_data}
-            A: 
+            Sleep data: {sleep_data}
             """)
         
-        system_message = "You are a multiple choice question answering system. Please use deductive logical reasoning to answer the given question."
+        system_message = "You are a sleep exper. Please analyse the given sleep data to give sleep hygiene suggestions to the patient."
         temperature_value = 0.2
         top_p_value = 1
         messages = [
@@ -34,7 +18,7 @@ class SleepAgent:
         ]
 
         # Set the OpenAI API key.
-        openai.api_key = get_env_var("MY_OPENAI_API_KEY")
+        openai.api_key = get_env_var("LIFE_UPGRADE_API_KEY")
 
         # Optional self-consistency approach returns early, selecting the answer choice with the highest frequency
         # return self.__self_consistency_response(messages, answer_choices, temperature_value, top_p_value, 5)
